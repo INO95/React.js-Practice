65999d067ae634c14f260bcc838774a0ea4028c4
INO95 <bih0905@naver.com>
2022-05-10 20:09:50 +0900

    220510

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import UserList from './UserList';
 import CreateUser from './CreateUser';
 
 // 커밋 이슈로 한번 수정함
+// 커밋 이슈로 한번 수정함
 
 function countActiveUsers(users) {
   console.log('활성 사용자 수를 세는 중...');
